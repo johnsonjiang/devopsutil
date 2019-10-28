a7f7311fb2b7ce68087de997d80275fc9e1fb7a8
Jiang Su <jiangsu@outlook.com>
2019-10-28 23:05:23 +0800

    Initial commit

---
+++ src/main/java/com/cde/devopsutil/DevopsutilApplication.java
@@ -0,0 +1,13 @@
+package com.cde.devopsutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsutilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cde/devopsutil/DevopsutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cde.devopsutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsutilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
