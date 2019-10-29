7444c2727a596bab8105c35c9ea363cf589021e1
Jiang Su <jiangsu@outlook.com>
2019-10-29 09:39:45 +0800

    Update DevopsutilApplication.java

--- src/main/java/com/cde/devopsutil/DevopsutilApplication.java
+++ src/main/java/com/cde/devopsutil/DevopsutilApplication.java
@@ -2,7 +2,6 @@ package com.cde.devopsutil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.cloud.*;
 
 @SpringBootApplication
 public class DevopsutilApplication {
