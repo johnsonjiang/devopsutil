542c2d44e45cd80d43130f9eda6f24b792d50a72
Jiang Su <jiangsu@outlook.com>
2019-10-30 14:03:26 +0800

    abc

--- src/main/java/com/cde/devopsutil/DevopsutilApplication.java
+++ src/main/java/com/cde/devopsutil/DevopsutilApplication.java
@@ -3,7 +3,7 @@ package com.cde.devopsutil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication;
+@SpringBootApplication;;
 public class DevopsutilApplication {
 
     public static void main(String[] args) {
